8120bc656ef380f4c031b25073ba4b54df251f7e
Paul Savvas <hello@paulsavvas.me>
2026-03-28 19:25:21 -0400

    Add MdxImage component and update image handling across multiple pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://paulsavvas.me',
   integrations: [react(), mdx(), sitemap()],
+  image: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'utfs.io' },
+      { protocol: 'https', hostname: '52k1cu2hr9.ufs.sh' },
+      { protocol: 'https', hostname: 'github.com' },
+    ],
+  },
   legacy: {
     collectionsBackwardsCompat: true,
   },
